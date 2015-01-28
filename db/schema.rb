f9e36481d6a73d631c42ee4282fba6da4e6fa53d
migs120 <migs432@yahoo.com>
2015-01-28 12:53:47 +0000

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20141212004050) do
 
-
-
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email",                  default: "", null: false
